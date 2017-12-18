dbeb681b10faa9ec654fcd4b82a7a46132c790fa
Godefroi Roussel <godefroi.roussel@gmail.com>
2017-12-18 12:30:43 +0100

    [Fix] Gestion du n avec le range

--- vertexCover.py
+++ vertexCover.py
@@ -8,14 +8,14 @@ from random import randint
 # Génération d'un graphe aléatoire à n sommets (avec un remplissage de 25% si possible)
 def init(n):
     G = nx.Graph()
-    G.add_nodes_from(range(1,n))
+    G.add_nodes_from(range(1,n+1))
     
     # Le nombre d'arêtes max est de (n*(n-1))/2 et on cherche un remplissage de 0.25%
     nbAretesMax = (n*(n-1))/2
     remplissageSouhaitee = 0.25 * nbAretesMax
     while (G.number_of_edges() < remplissageSouhaitee): #Remplissage est inférieur à 25% on ajoute des arêtes
-        noeud1 = randint(1, n-1)
-        noeud2= randint(1, n-1)
+        noeud1 = randint(1, n)
+        noeud2= randint(1, n)
         G.add_edge(noeud1, noeud2)
     print(list(G.edges()))
     print(list(G.nodes()))
