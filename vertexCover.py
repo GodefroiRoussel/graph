c4c01c9779532117d9c7a28a7ee7f3c059c9a398
Godefroi Roussel <godefroi.roussel@gmail.com>
2017-12-18 11:48:29 +0100

    [Add] fichier vertex cover

---
+++ vertexCover.py
@@ -0,0 +1,12 @@
+
+
+
+
+# Génération d'un graphe aléatoire à n sommets (avec un remplissage de 25% si possible)
+
+
+
+
+#En sortie : le min VC (via une recherche exhaustive) et min VC (avec une 2-approx via le MaxMatching)
+
+
